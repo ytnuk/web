aab6649f1227e63c61ad945aeef3c41a2ed154f0
Vít Kutný <vitkutny@outlook.com>
2014-11-20 23:58:45 +0100

    dev

--- src/Web/Extension.php
+++ src/Web/Extension.php
@@ -17,13 +17,11 @@ final class Extension extends Nette\DI\CompilerExtension implements WebEdit\Conf
 	 * @var array
 	 */
 	private $defaults = [
-		'mask' => '[<locale [a-z]{2}_[A-Z]{2}?>/]<module>[/<action>][/<id [0-9]+>]',
-		'metadata' => [
-			'module' => 'Home',
-			'presenter' => 'Presenter',
-			'action' => 'view',
-			'locale' => 'en_US',
-		],
+		'web' => NULL,
+		'module' => 'Home',
+		'presenter' => 'Presenter',
+		'action' => 'view',
+		'locale' => 'en_US',
 	];
 
 	/**
@@ -32,24 +30,26 @@ final class Extension extends Nette\DI\CompilerExtension implements WebEdit\Conf
 	public function getConfigResources()
 	{
 		$config = $this->getConfig($this->defaults);
-		$config['metadata']['web'] = $this->name;
-
-		return [
-			Nette\Bridges\ApplicationDI\ApplicationExtension::class => [
-				'mapping' => [
-					'*' => ucfirst($this->name) . '\*\*'
-				]
-			],
+		$configResources = [
 			Nette\Bridges\ApplicationDI\RoutingExtension::class => [
 				'routes' => [
-					$config['mask'] => $config['metadata']
-				]
-			],
-			WebEdit\Alias\Extension::class => [
-				'pattern' => [
-					'WebEdit\*' => ucfirst($this->name) . '\\$1'
+					'//' . $this->name . '/[<locale [a-z]{2}_[A-Z]{2}?>/]<module>[/<action>][/<id [0-9]+>]' => $config
 				]
 			]
 		];
+		if ($_SERVER['SERVER_NAME'] === $this->name) {
+			$configResources[Nette\Bridges\ApplicationDI\ApplicationExtension::class] = [
+				'mapping' => [
+					'*' => ucfirst($config['web']) . '\*\*'
+				]
+			];
+			$configResources[WebEdit\Alias\Extension::class] = [
+				'pattern' => [
+					'WebEdit\*' => ucfirst($config['web']) . '\\$1'
+				]
+			];
+		}
+
+		return $configResources;
 	}
 }
