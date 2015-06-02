09b55fa4c0e5df874f75d52c3d4ed6150d6ccd47
Vít Kutný <vitkutny@icloud.com>
2015-06-03 00:00:20 +0200

    dev

--- src/Web/Entity.php
+++ src/Web/Entity.php
@@ -8,7 +8,7 @@ use Ytnuk;
 /**
  * @property string $project
  * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Translation\Entity $name {1:1d Ytnuk\Translation\Repository $web primary}
- * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Menu\Entity $menu {1:1d Ytnuk\Menu\Repository $page primary}
+ * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Menu\Entity $menu {1:1d Ytnuk\Menu\Repository $web primary}
  */
 final class Entity extends Ytnuk\Orm\Entity
 {
