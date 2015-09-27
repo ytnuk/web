1f15575a6d6f60bf1f30f22fdfe687029b84bf01
Vít Kutný <vitkutny@icloud.com>
2015-09-27 17:42:17 +0200

    nextras/orm entity metadata: introduced new parser
    https://github.com/nextras/orm/commit/dfcbf5291eaa64559a8f8c8836f308573b99e549

--- src/Web/Entity.php
+++ src/Web/Entity.php
@@ -6,8 +6,8 @@ use Ytnuk;
 
 /**
  * @property string $id
- * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Translation\Entity $name {1:1d Ytnuk\Translation\Entity::$web primary}
- * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Menu\Entity $menu {1:1d Ytnuk\Menu\Entity::$web primary}
+ * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Translation\Entity $name {1:1d Ytnuk\Translation\Entity::$web, primary=true}
+ * @property Nextras\Orm\Relationships\OneHasOneDirected|Ytnuk\Menu\Entity $menu {1:1d Ytnuk\Menu\Entity::$web, primary=true}
  * @property Nextras\Orm\Relationships\OneHasMany|Locale\Entity[] $localeNodes {1:m Locale\Entity::$web}
  * @property-read array|Ytnuk\Translation\Locale\Entity[] $locales {virtual}
  * @property-read Ytnuk\Translation\Locale\Entity|NULL $locale {virtual}
