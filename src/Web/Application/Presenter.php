9451921e7f5f04868934c34ffcba6adfff1d59d9
Vít Kutný <vitkutny@icloud.com>
2015-11-11 01:19:35 +0100

    foo

--- src/Web/Application/Presenter.php
+++ src/Web/Application/Presenter.php
@@ -63,7 +63,7 @@ abstract class Presenter
 		}
 	}
 
-	//TODO: should not be used for accesing menu, create directly menu control using multiplier and access directly using an identifier
+	//TODO: should not be used for accessing menu, create directly menu control using multiplier and access directly using an identifier
 	protected function createComponentWeb() : Ytnuk\Web\Control
 	{
 		return $this->control->create($this->entity);
