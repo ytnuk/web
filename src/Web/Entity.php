0190d85de291ffdd238dcbe36fe64efbeecf28a4
Vít Kutný <vitkutny@icloud.com>
2015-12-01 23:53:25 +0100

    translation cascade remove

--- src/Web/Entity.php
+++ src/Web/Entity.php
@@ -6,7 +6,7 @@ use Ytnuk;
 
 /**
  * @property string $id {primary}
- * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Translation\Entity $name {1:1 Ytnuk\Translation\Entity::$web, primary=true}
+ * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Translation\Entity $name {1:1 Ytnuk\Translation\Entity::$web, primary=true, cascade=[persist, remove]}
  * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Menu\Entity $menu {1:1 Ytnuk\Menu\Entity::$web, primary=true}
  * @property Nextras\Orm\Relationships\OneHasMany|Domain\Entity[] $domains {1:m Domain\Entity::$web}
  */
