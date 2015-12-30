3204c9ebe45e82ae351c45cf6eb816dcc70b23e3
Vít Kutný <vitkutny@icloud.com>
2015-12-30 23:20:20 +0100

    nextras/orm: one sided relations, primary => isMain

--- src/Web/Domain/Locale/Entity.php
+++ src/Web/Domain/Locale/Entity.php
@@ -7,7 +7,7 @@ use Ytnuk;
 /**
  * @property int $id {primary}
  * @property Nextras\Orm\Relationships\ManyHasOne|Ytnuk\Web\Domain\Entity $domain {m:1 Ytnuk\Web\Domain\Entity::$localeNodes}
- * @property Nextras\Orm\Relationships\ManyHasOne|Ytnuk\Translation\Locale\Entity $locale {m:1 Ytnuk\Translation\Locale\Entity::$webDomainNodes}
+ * @property Nextras\Orm\Relationships\ManyHasOne|Ytnuk\Translation\Locale\Entity $locale {m:1 Ytnuk\Translation\Locale\Entity, oneSided=true}
  * @property bool|NULL $primary
  */
 final class Entity
--- src/Web/Entity.php
+++ src/Web/Entity.php
@@ -7,8 +7,8 @@ use Ytnuk;
 /**
  * @property int $id {primary}
  * @property string $alias
- * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Translation\Entity $name {1:1 Ytnuk\Translation\Entity::$web, primary=true, cascade=[persist, remove]}
- * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Menu\Entity $menu {1:1 Ytnuk\Menu\Entity::$web, primary=true}
+ * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Translation\Entity $name {1:1 Ytnuk\Translation\Entity, oneSided=true, isMain=true, cascade=[persist, remove]}
+ * @property Nextras\Orm\Relationships\OneHasOne|Ytnuk\Menu\Entity $menu {1:1 Ytnuk\Menu\Entity, oneSided=true, isMain=true}
  * @property Nextras\Orm\Relationships\OneHasMany|Domain\Entity[] $domains {1:m Domain\Entity::$web}
  */
 final class Entity
